53f2b8e7bb8aa1a85e13f307f20a1f3926eba9ab
Sergey Bedulin <serge.bedulin@gmail.com>
2016-01-13 15:59:06 +0300

    First GraphQL Server

---
+++ data/schema.js
@@ -0,0 +1,24 @@
+import {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString
+} from 'graphql';
+
+let schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Query',
+        fields: () => ({
+            counter: {
+                type: GraphQLInt,
+                resolve: () => 42
+            },
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello GraphQL'
+            }
+        })
+    })
+});
+
+export default schema;
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import schema from './data/schema';
+import GraphQLHTTP from 'express-graphql';
 import { MongoClient } from 'mongodb';
 
 let app = express();
 let port = 3000;
 
 app.use(express.static('public'));
+app.use('/graphql', GraphQLHTTP({
+    schema: schema,
+    graphiql: true
+}));
 
 let db;
 MongoClient.connect(process.env.MONGO_URL, (err, database) => {
